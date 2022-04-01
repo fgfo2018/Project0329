9da0e24146892477315300b4fb258ec0fd7fed21
fgfo2018 <34053331+fgfo2018@users.noreply.github.com>
2022-04-01 11:28:11 +0800

    louis 0331_v12

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ var config = {};
 // server01 setting (webserver)
 config.server01_Url = "rtsp://192.168.0.176/avc" 
 config.server01_Port = 6147
-config.server01_VIDEO_FPS = 1
+config.server01_VIDEO_FPS = 30
 // server02 setting
-config.server02_Url = "rtsp://192.168.0.176/avc"
+config.server02_Url = "rtsp://192.168.0.181/avc"
 config.server02_Port = 6148 
-config.server02_VIDEO_FPS = 1
+config.server02_VIDEO_FPS = 30
 // server03 setting
 config.server03_Url = "rtsp://192.168.0.176/avc"
 config.server03_Port = 6149 
-config.server03_VIDEO_FPS = 1
+config.server03_VIDEO_FPS = 30
 module.exports = config;
