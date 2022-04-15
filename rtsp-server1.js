1f0aad0002b2c02f8b07e96f111357807ef7d3e4
fgfo2018 <34053331+fgfo2018@users.noreply.github.com>
2022-04-15 09:51:13 +0800

    louis 0415_v1

--- rtsp-server1.js
+++ rtsp-server1.js
@@ -54,9 +54,45 @@ app.get('/config', function (req, res) {
     // console.log();
     res.send(output)
 });
-
+var testtt = 0
+app.post('/log', function (req, res) {
+    const id = JSON.parse(req.body)
+    var data = '[ERROR][' + mToday() + ']:video is not working' + id.name;
+    console.log(data)
+    saveLogData(data)
+    testtt++
+    res.send({
+        success: true,
+    })
+});
 const getSpotsData = () => {
     const jsonData = fs.readFileSync('./config.json')
     // console.log(JSON.parse(jsonData))
     return JSON.parse(jsonData)
+}
+const saveLogData = (content) => {
+    const path = './log.txt'
+    try {
+        if (!fs.existsSync(path)) {
+            fs.writeFile(path, '', function (error) {
+                console.log(error)
+                console.log('create log.txt')
+            })
+        }
+    } catch (err) {
+        console.error(err)
+    }
+    var data = fs.readFileSync(path).toString().split("\n");
+    data.splice(-1, 0, content);
+    var text = data.join("\n");
+    fs.writeFile(path, text, function (err) {
+        if (err) return console.log(err);
+    });
+}
+
+function mToday() {
+    var m = new Date().toLocaleString('zh-TW', {
+        timeZone: 'Asia/Taipei'
+    });
+    return m;
 }
